7976626fc231418d374d579a30f0e8d8f984342c
MOHAMED-ALI-YOUSSOUF <wizzimed@gmail.com>
2025-02-08 00:59:44 +0300

    lms

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+
+      },
+      {
+        hostname: "cdn.sanity.io",
+        protocol: "https",
+      },
+      {
+        hostname: "img.clerk.com",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
